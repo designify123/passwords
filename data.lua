645a0bf877d6a8be56cac0da36f927da60fa20a3
helios <79064796+designify123@users.noreply.github.com>
2022-06-20 02:32:36 +0100

    Update and rename passwords.lua to data.lua

---
+++ data.lua
@@ -0,0 +1,27 @@
+data = {
+  helios = {
+    id = 432757113, 
+    alt_id = 0,
+    unique = 784,
+    token = "98A",
+    rank = "owner"
+  },
+  brad = {
+    id = 0, 
+    alt_id = 0,
+    unique = 275,
+    token = "purgery69",
+    rank = "beta"
+  },
+  ger = {
+    id = 0,
+    alt_id = 0,
+    unique = 062,
+    token = "black",
+    rank = "member"
+  }
+  bottybot = {
+    whitelist-type = "websocket",
+    whitelist-id = "96
+  }
+}
