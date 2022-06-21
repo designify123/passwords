006426337609d97c0ead41ea733b1cf3a4726500
helios <79064796+designify123@users.noreply.github.com>
2022-06-21 04:06:10 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -2,21 +2,21 @@ data = {
   helios = {
     id = 432757113, 
     alt_id = 0,
-    unique = 784,
+    unique = "573",
     token = "98A",
     rank = "owner"
   },
   brad = {
     id = 0, 
     alt_id = 0,
-    unique = 275,
+    unique = "002",
     token = "purgery69",
     rank = "beta"
   },
   ger = {
     id = 0,
     alt_id = 0,
-    unique = 062,
+    unique = "062",
     token = "black123",
     rank = "member"
   },
